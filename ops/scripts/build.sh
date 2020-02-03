efc32d0266fc3ae49c213e8f374ef763d02243c0
Eathan Langford <eathan.langford@myob.com>
2020-02-04 09:54:54 +1100

    pushing to ecr

--- ops/scripts/build.sh
+++ ops/scripts/build.sh
@@ -1,2 +1,4 @@
 echo "BUILDING IMAGE"
 docker build -t eathan-hello-world . -f ./ops/dockerFiles/Dockerfile.build
+docker tag eathan-hello-world:latest 945367126992.dkr.ecr.ap-southeast-2.amazonaws.com/eathan-hello-world
+docker push 945367126992.dkr.ecr.ap-southeast-2.amazonaws.com/eathan-hello-world:latest
