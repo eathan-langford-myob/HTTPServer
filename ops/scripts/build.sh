d48d6a389ceda8c146b30ec5554970ebcebeacf5
Eathan Langford <eathan.langford@myob.com>
2020-02-05 11:36:02 +1100

    testing build

--- ops/scripts/build.sh
+++ ops/scripts/build.sh
@@ -1,17 +1,11 @@
 tag=$1
-ECR_URL="945367126992.dkr.ecr.ap-southeast-2.amazonaws.com"
-ECR_Name="eathan-hello-world"
-ECR="${ECR_URL}/${ECR_Name}"
-Dockerfile="./ops/dockerFiles/Dockerfile.build"
+
 
 echo "---------------------GETTING AWS AUTH---------------------"
 $(aws ecr get-login --no-include-email --region ap-southeast-2)
-
 echo "---------------------BUILDING IMAGE---------------------"
-docker build -t "${ECR_Name}:${tag}" . -f "${Dockerfile}"
-
+docker build -t eathan-hello-world . -f ./ops/dockerFiles/Dockerfile.build
 echo "---------------------TAGGING IMAGE---------------------"
-docker tag "${ECR_Name}:${tag}" "${ECR}"
-
+docker tag eathan-hello-world:latest 945367126992.dkr.ecr.ap-southeast-2.amazonaws.com/eathan-hello-world
 echo "---------------------PUSHING IMAGE---------------------"
-docker push "${ECR}:${tag}"
+docker push 945367126992.dkr.ecr.ap-southeast-2.amazonaws.com/eathan-hello-world:latest
