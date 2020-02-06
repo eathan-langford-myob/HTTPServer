3b319899f8bbba2fdcaa9f893849f72f7a5facbb
Eathan Langford <eathan.langford@myob.com>
2020-02-06 13:33:32 +1100

    implementing build number from buildkite

--- ops/scripts/build.sh
+++ ops/scripts/build.sh
@@ -1,11 +1,8 @@
-tag=$1
-echo ${BUILDKITE_BUILD_NUMBER}
-
 echo "---------------------GETTING AWS AUTH---------------------"
 $(aws ecr get-login --no-include-email --region ap-southeast-2)
 echo "---------------------BUILDING IMAGE---------------------"
 docker build -t eathan-hello-world . -f ./ops/dockerFiles/Dockerfile.build
 echo "---------------------TAGGING IMAGE---------------------"
-docker tag eathan-hello-world:latest 945367126992.dkr.ecr.ap-southeast-2.amazonaws.com/eathan-hello-world
+docker tag eathan-hello-world:${BUILDKITE_BUILD_NUMBER} 945367126992.dkr.ecr.ap-southeast-2.amazonaws.com/eathan-hello-world
 echo "---------------------PUSHING IMAGE---------------------"
-docker push 945367126992.dkr.ecr.ap-southeast-2.amazonaws.com/eathan-hello-world:latest
+docker push 945367126992.dkr.ecr.ap-southeast-2.amazonaws.com/eathan-hello-world:${BUILDKITE_BUILD_NUMBER}
--- ops/scripts/deploy.sh
+++ ops/scripts/deploy.sh
@@ -1,4 +1,4 @@
 #BUILD_NUMBER=$1
 
 echo "-----------------DEPLOYING TO JUPITER-----------------"
-ktmpl ./ops/deploy/deployment.yml -f ./ops/deploy/default.yml -p imageTag latest | kubectl apply -f -
+ktmpl ./ops/deploy/deployment.yml -f ./ops/deploy/default.yml -p imageTag ${BUILDKITE_BUILD_NUMBER} | kubectl apply -f -
