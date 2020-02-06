e18b1fd2fbd79883c40db68302d8b08d11f92b95
Eathan Langford <eathan.langford@myob.com>
2020-02-06 15:34:52 +1100

    implementing build number from buildkite

--- ops/scripts/deploy.sh
+++ ops/scripts/deploy.sh
@@ -1,4 +1,2 @@
-#BUILD_NUMBER=$1
-
 echo "-----------------DEPLOYING TO JUPITER-----------------"
 ktmpl ./ops/deploy/deployment.yml -f ./ops/deploy/default.yml -p imageTag ${BUILDKITE_BUILD_NUMBER} | kubectl apply -f -
--- ops/scripts/docker.sh
+++
@@ -1,3 +0,0 @@
-$(aws ecr get-login --no-include-email --region ap-southeast-2)
-docker pull 945367126992.dkr.ecr.ap-southeast-2.amazonaws.com/eathan-hello-world:latest
-docker run -p 3333:8080 eathan-hello-world:latest
