c847c91b2e01b9c3ae3b0871aaedf548e92b0079
Eathan Langford <eathan.langford@myob.com>
2020-02-06 14:15:37 +1100

    implementing build number from buildkite

--- ops/scripts/build.sh
+++ ops/scripts/build.sh
@@ -1,7 +1,7 @@
 echo "---------------------GETTING AWS AUTH---------------------"
 $(aws ecr get-login --no-include-email --region ap-southeast-2)
 echo "---------------------BUILDING IMAGE---------------------"
-docker build -t eathan-hello-world . -f ./ops/dockerFiles/Dockerfile.build
+docker build -t eathan-hello-world:${BUILDKITE_BUILD_NUMBER} . -f ./ops/dockerFiles/Dockerfile.build
 echo "---------------------TAGGING IMAGE---------------------"
 docker tag eathan-hello-world:${BUILDKITE_BUILD_NUMBER} 945367126992.dkr.ecr.ap-southeast-2.amazonaws.com/eathan-hello-world
 echo "---------------------PUSHING IMAGE---------------------"
