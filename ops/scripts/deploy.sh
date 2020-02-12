527eb6af189741dc8db77862b00d0003ab49fe02
Eathan Langford <eathan.langford@myob.com>
2020-02-13 10:53:26 +1100

    clean up files, add kube line in deploy script

--- ops/scripts/deploy.sh
+++ ops/scripts/deploy.sh
@@ -1,4 +1,3 @@
+#!/usr/bin/env bash
 echo "-----------------DEPLOYING TO JUPITER-----------------"
-ktmpl ./ops/deploy/deployment.yml -f ./ops/deploy/default.yml -p imageTag ${BUILDKITE_BUILD_NUMBER}
-echo "-----------------READINESS-----------------"
-kubectl rollout status deployment.v1.apps/eathan-hello-world-deploy --timeout=60s
+ktmpl ./ops/deploy/deployment.yml -f ./ops/deploy/default.yml -p imageTag ${BUILDKITE_BUILD_NUMBER} | kubectl apply -f -
